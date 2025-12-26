4756f751b56d04cf278b845c2471de8d8b6ce8e8
Gabriel <gzelaya0404@gmail.com>
2025-12-25 21:28:53 -0600

    UX improvement for loading spinner (#2)

--- src/components/LoadingSpinner/LoadingSpinner.jsx
+++ src/components/LoadingSpinner/LoadingSpinner.jsx
@@ -3,10 +3,13 @@ import CircularProgress from "@mui/material/CircularProgress";
 
 export default function LoadingSpinner() {
   return (
+    <>
     <div className="w-full flex justify-center pt-12">
       <Stack>
         <CircularProgress sx={{ color: "#00fe8c" }} />
       </Stack>
     </div>
+    <div className="w-full flex justify-center pt-12 text-center text-white mb-200 font-bold text-lg">The initial response may take up to 20 seconds. Thank you for your patience.</div>
+    </>
   );
 }
